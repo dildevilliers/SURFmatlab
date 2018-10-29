0df8c7a5ba71d9cdf238cbc9799125b14f0dd8d3
De Villiers <ddv@sun.ac.za>
2018-10-29 22:41:08 +0200

    Reworked interpolation and main plot routines significantly.  Better now...

--- @FarField/interpolateGrid.m
+++ @FarField/interpolateGrid.m
@@ -16,28 +16,35 @@ typeValidationXY = @(x) validateattributes(x,{'numeric'},{'real','nonempty'},'in
 addRequired(parseobj,'xi',typeValidationXY);
 addRequired(parseobj,'yi',typeValidationXY);
 
-expectedgridType = {'PhTh','UV','AzEl','ElAz','TrueView','ArcSin'};
-addOptional(parseobj,'gridType',obj.gridType, @(x) any(validatestring(x,expectedgridType)));
+% expectedgridType = {'PhTh','DirCos','AzEl','ElAz','TrueView','ArcSin'};
+% addOptional(parseobj,'gridType',obj.gridType, @(x) any(validatestring(x,expectedgridType)));
 
 typeValidationFreq = @(x) validateattributes(x,{'numeric'},{'real','nonempty','integer'},'interpolateGrid','freqIndex');
 addOptional(parseobj,'freqIndex',1,typeValidationFreq);
 
 parse(parseobj,obj,output,xi,yi,varargin{:});
 
-gridType = parseobj.Results.gridType;
+% gridType = parseobj.Results.gridType;
 freqIndex = parseobj.Results.freqIndex;
 
 %% Main code
 
-obj = obj.resetToBase;
+gridType = obj.gridType;
+
+% Evaluate the field on the base grid - this is where the output function
+% should be best suited for interpolation
+obj = obj.grid2Base;
 
 % Get xi and yi in the base gridType
 if ~strcmp(obj.gridType,gridType)
-    grid2UVhandle = str2func(['FarField.',gridType,'2UV']);
-    [u,v,w] = grid2UVhandle(xi,yi);
+    % Shift to -180:180 range (if applicable)
+    obj = obj.xRange180180;
+
+    grid2DirCoshandle = str2func(['FarField.',gridType,'2DirCos']);
+    [u,v,w] = grid2DirCoshandle(xi,yi);
     
-    UV2baseHandle = str2func(['FarField.UV2',obj.gridTypeBase]);
-    [xi_bGT,yi_bGT] = UV2baseHandle(u,v,w);
+    DirCos2baseHandle = str2func(['FarField.DirCos2',obj.gridTypeBase]);
+    [xi_bGT,yi_bGT] = DirCos2baseHandle(u,v,w);
 else
     xi_bGT = xi;
     yi_bGT = yi;
@@ -59,7 +66,7 @@ end
 Z = Zfreq(:,freqIndex);
 
 % Build the interpolant on the base grid
-Zf = scatteredInterpolant(obj.x,obj.y,Z,'linear','linear');
+Zf = scatteredInterpolant(obj.x,obj.y,Z,'natural');
 % Get the values on the scattered set of inputs
 Zi = Zf(xi_bGT,yi_bGT);
 
