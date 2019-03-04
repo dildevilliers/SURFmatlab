60dc85a2525479f7db8c33bb064dcd57f6b4ae3e
brandtklopper <15707776@sun.ac.za>
2019-03-04 10:52:09 +0200

    added a bit to test script for farFieldFromPowerPattern

--- testScripts/testScript_farFieldFromPowerPattern.m
+++ testScripts/testScript_farFieldFromPowerPattern.m
@@ -14,26 +14,58 @@ P1 = powerPattern(TH(:),PH(:),'gauss',th0,taper_dB,freq);
 %Build example FarField objects from P1 power pattern (different
 %polarizations)
 
-FF1 = FarField.farFieldFromPowerPattern(TH(:),PH(:),P1,freq,'linearX');
-figure;
-%E1 is -inf dB (perfectly X-linear field in Ludwig3 coords) and will break plot
-FF1.plot('plotType','2D','output','E1','outputType','mag','step',1,'showGrid',true,'dynamicRange_dB',30);
-% FF1.plot('plotType','2D','output','E2','outputType','mag','step',1,'showGrid',true,'dynamicRange_dB',30) 
+[plotlinX plotlinY plotcircLH plotcircRH] = deal([1 1 1 1]);
 
-FF2 = FarField.farFieldFromPowerPattern(TH(:),PH(:),P1,freq,'linearY');
-figure;
-%E1 is -inf dB (perfectly Y-linear field in Ludwig3 coords) and will break plot
-% FF2.plot('plotType','2D','output','E1','outputType','mag','step',1,'showGrid',true,'dynamicRange_dB',30); 
-FF2.plot('plotType','2D','output','E2','outputType','mag','step',1,'showGrid',true,'dynamicRange_dB',30);
+if plotlinX
+    FF1 = FarField.farFieldFromPowerPattern(TH(:),PH(:),P1,freq,'linearX');
+    figure
+    %E1 is -inf dB (perfectly X-linear field in Ludwig3 coords) and will break plot
+    subplot(3,1,1)
+    FF1.plot('plotType','2D','output','E1','outputType','mag','step',1,'showGrid',true,'dynamicRange_dB',30);
+    FF1 = coor2spherical(FF1);
+    subplot(3,1,2)
+    FF1.plot('plotType','2D','output','E1','outputType','mag','step',1,'showGrid',true,'dynamicRange_dB',30);
+    subplot(3,1,3)
+    FF1.plot('plotType','2D','output','E2','outputType','mag','step',1,'showGrid',true,'dynamicRange_dB',30);
+    % FF1.plot('plotType','2D','output','E2','outputType','mag','step',1,'showGrid',true,'dynamicRange_dB',30)
+end
 
-FF3 = FarField.farFieldFromPowerPattern(TH(:),PH(:),P1,freq,'circularLH');
-figure
-%E2 is -inf dB (perfectly LH circular field in spherical pol basis) and will break plot
-FF3.plot('plotType','2D','output','E1','outputType','mag','step',1,'showGrid',true,'dynamicRange_dB',30);
-% FF3.plot('plotType','2D','output','E2','outputType','mag','step',1,'showGrid',true,'dynamicRange_dB',30);
+if plotlinY
+    FF2 = FarField.farFieldFromPowerPattern(TH(:),PH(:),P1,freq,'linearY');
+    figure
+    %E1 is -inf dB (perfectly Y-linear field in Ludwig3 coords) and will break plot
+    % FF2.plot('plotType','2D','output','E1','outputType','mag','step',1,'showGrid',true,'dynamicRange_dB',30);
+    subplot(3,1,1)
+    FF2.plot('plotType','2D','output','E2','outputType','mag','step',1,'showGrid',true,'dynamicRange_dB',30);
+    FF2 = coor2spherical(FF2);
+    subplot(3,1,2)
+    FF2.plot('plotType','2D','output','E1','outputType','mag','step',1,'showGrid',true,'dynamicRange_dB',30);
+    subplot(3,1,3)
+    FF2.plot('plotType','2D','output','E2','outputType','mag','step',1,'showGrid',true,'dynamicRange_dB',30);
+end
 
-FF4 = FarField.farFieldFromPowerPattern(TH(:),PH(:),P1,freq,'circularRH');
-figure
-%E1 is -inf dB (perfectly RH circular field in spherical pol basis) and will break plot
-% FF4.plot('plotType','2D','output','E1','outputType','mag','step',1,'showGrid',true,'dynamicRange_dB',30);
-FF4.plot('plotType','2D','output','E2','outputType','mag','step',1,'showGrid',true,'dynamicRange_dB',30);
+if plotcircLH
+    FF3 = FarField.farFieldFromPowerPattern(TH(:),PH(:),P1,freq,'circularLH');
+    figure
+    %E2 is -inf dB (perfectly LH circular field in spherical pol basis) and will break plot
+    subplot(3,1,1)
+    FF3.plot('plotType','2D','output','E1','outputType','mag','step',1,'showGrid',true,'dynamicRange_dB',30);
+    FF3 = pol2linear(FF3);
+    subplot(3,1,2)
+    FF3.plot('plotType','2D','output','E1','outputType','mag','step',1,'showGrid',true,'dynamicRange_dB',30);
+    subplot(3,1,3)
+    FF3.plot('plotType','2D','output','E2','outputType','mag','step',1,'showGrid',true,'dynamicRange_dB',30);
+end
+
+if plotcircRH
+    FF4 = FarField.farFieldFromPowerPattern(TH(:),PH(:),P1,freq,'circularRH');
+    figure
+    %E1 is -inf dB (perfectly RH circular field in spherical pol basis) and will break plot
+    subplot(3,1,1)
+    FF3.plot('plotType','2D','output','E2','outputType','mag','step',1,'showGrid',true,'dynamicRange_dB',30);
+    FF3 = pol2linear(FF3);
+    subplot(3,1,2)
+    FF3.plot('plotType','2D','output','E1','outputType','mag','step',1,'showGrid',true,'dynamicRange_dB',30);
+    subplot(3,1,3)
+    FF3.plot('plotType','2D','output','E2','outputType','mag','step',1,'showGrid',true,'dynamicRange_dB',30);
+end
