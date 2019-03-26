6f82f1f3b9600eca68ae1ca87af7b216e4e8a78c
De Villiers <ddv@sun.ac.za>
2019-03-26 23:10:32 +0200

    PlaneWaveSignal fixed power, ArrayElements and ArrayReceiver basic functionality implemented

--- Array/@ArrayElements/ArrayElements.m
+++ Array/@ArrayElements/ArrayElements.m
@@ -3,20 +3,57 @@ classdef ArrayElements
     %   Detailed explanation goes here
     
     properties
-        Property1
+        antPos(1,1) pnt3D = pnt3D([0,1],0,0)   % Antenna positions in 3D points - internal vector
+        channelErrors(1,:) double {mustBeFinite} = 1 % Vector of complex channel errors for calibration testing
+    end
+    
+    properties (SetAccess = private)
+       N_elements     % Number of elements
     end
     
     methods
-        function obj = ArrayElements(inputArg1,inputArg2)
-            %ARRAYELEMENTS Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
+        function obj = ArrayElements(antPos,channelErrors)
+            if nargin >= 1
+                obj.antPos = antPos;
+            end
+            if nargin >= 2
+                obj.channelErrors = channelErrors;
+            end
+            obj.N_elements = length(obj.antPos.x);
+            if length(obj.channelErrors) == 1
+                obj.channelErrors = repmat(obj.channelErrors,1,obj.N_elements);
+            else 
+                assert(length(obj.channelErrors) == obj.N_elements,'Length of channelErrors should match the number of elements');
+            end
         end
         
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
+        function portSigMat = portSignals(obj,S,t)
+            % Takes an array of PlaneWaveSignal objects (in S), adds them up, and
+            % calculates the complex analog signals at the array antenna ports  at the time samples t 
+            if nargin == 1
+                S = PlaneWaveSignal;
+                t = linspace(0,10,1001);
+            end
+            assert(min(size(t)) == 1,'Error, t must be a vector, not a matrix');
+            assert(isreal(t),'Error, t must be real');
+            t = t(:);
+            Nsig = length(S);
+            % Build the signal matrix
+            si = zeros(length(t),Nsig);
+            k = zeros(3,Nsig);
+            for ss = 1:Nsig
+                si(:,ss) = S(ss).getSignal(t);
+                k(:,ss) = S(ss).k;
+            end
+            % Get the element phase shifts
+            r = obj.antPos.pointMatrix.';  % [Nant x 3]
+            A = exp(-1i*r*k);              % [Nant x Nsig]
+            
+            % Add the systematic errors in the channels
+            AC = bsxfun(@times,A,obj.channelErrors(:));
+            
+            % Multiply the signals through the channels
+            portSigMat = AC*si.';
         end
     end
 end
