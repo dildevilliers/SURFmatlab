e15aa9d5c53e473084e504f36ce0fd35f0f4ea36
Ridalise <ridalise@gmail.com>
2019-04-03 14:05:14 +0200

    Adding Farfield testscript

--- testScripts/testScript_FarField.m
+++ testScripts/testScript_FarField.m
@@ -1,66 +1,51 @@
 %Name: testScript_FarField.m
 %Description:
-%   Script to test the Farfield object. A default Farfield is created and the
-%   use cases of its various methods are shown.
-
-% A default FarField class :
-% Generates a Gaussian beam pattern at a single frequency (1 GHz) over the full sphere, 5 degree angular resolution
+%   Script to test the Farfield object. A patch antenna Farfield is read in from a FEKO ffe file
+%   and the use cases of its various methods are shown.
 
 close all
-
-% FF = FarField;
 FF = FarField.readFEKOffe([pwd, '\testScripts\patch']);
 
-%FF = FF.pol2circular();
-% figure 
-% FF.plot;
-
-gridType = 'AzEl';
-coorType = 'Ludwig3';
-polType = 'circular';
+gridType = {'PhTh' 'DirCos' 'AzEl' 'ElAz' 'TrueView' 'ArcSin'}; 
+coorType = {'spherical' 'Ludwig1' 'Ludwig2AE' 'Ludwig2EA' 'Ludwig3'};
+polType = {'linear' 'circular' 'slant'};
 xRangeType = 'pos';
 
-handleGridType = str2func(['grid2',gridType]);
-handleCoorType = str2func(['coor2',coorType]);
-handlePolType = str2func(['pol2',polType]);
-FF = handleGridType(FF);
-FF = handleCoorType(FF,0);
-FF = handlePolType(FF);
-FF = FF.setXrange(xRangeType);
+for i = 1:6
+    handleGridType = str2func(['grid2',gridType{i}]);
+    FF = handleGridType(FF);
+    figure
+    plotGrid(FF);
+end
+
+for i = 1:6
+    k = i;
+    if i > 5
+        k = 1;
+    end
+    for j = 1:3
+        handleGridType = str2func(['grid2',gridType{i}]);
+        handleCoorType = str2func(['coor2',coorType{k}]);
+        handlePolType = str2func(['pol2',polType{j}]);
+        FF = handleGridType(FF);
+        FF = handleCoorType(FF,0);
+        FF = handlePolType(FF);
+        FF = FF.setXrange(xRangeType);
+        figure
+        FF.plot('plotType','2D','step',1,'showGrid',true,'output','E1','outputType','mag');
+        figure
+        FF.plot('plotType','2D','step',1,'showGrid',true,'output','E2','outputType','mag');
+%         figure
+%         FF.plot('plotType','2D','step',1,'showGrid',true,'output','CO_XP','outputType','mag');
+%         figure
+%         FF.plot('plotType','2D','step',1,'showGrid',true,'output','XP_CO','outputType','mag');
+    end  
+end
+
 figure
-%FF.plot('plotType','2D','step',1,'showGrid',true,'output','E1','outputType','mag');
-% figure
-% FF.plot('plotType','2D','step',1,'showGrid',true,'output','E2','outputType','mag');
+plotType = 'polar'; % '3D', '2D', 'polar', 'cartesian'
+FF.plot('plotType', plotType);
 
-% figure
-% plotType = 'polar'; % '3D', '2D', 'polar', 'cartesian'
-% FF.plot('plotType', plotType);
-
-% figure
-% FF.plotPrincipleCuts;
-% 
- figure 
- %FF.plot('plotType', 'polar', 'output', 'CO_XP', 'outputType', 'mag')
-
-FF2 = grid2PhTh(FF);
 figure
-plotGrid(FF2);
-FF.plot();
-% 
-% FF3 = grid2AzEl(FF);
-% figure
-% plotGrid(FF3);
-% 
-% FF4 = grid2ArcSin(FF);
-% figure
-% plotGrid(FF4);
-% 
-% FF5 = grid2ElAz(FF);
-% figure
-% plotGrid(FF5);
-% 
-% FF6 = grid2TrueView(FF);
-% figure
-% plotGrid(FF6);
-
+FF.plotPrincipleCuts;
 
